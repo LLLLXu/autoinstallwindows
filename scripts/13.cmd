c467de8007a262b1520d9c9a07b73e91d183bc25
unknown <lx_forest@hotmail.com>
2024-06-18 16:00:35 +0800

    202406181600

--- scripts/13.cmd
+++ scripts/13.cmd
@@ -1,3 +1,4 @@
 @REM @echo off
+start /min c:\download\scripts\disablenetwork.cmd
 start /wait c:\download\software\office\setup.exe /configure c:\download\software\office\All2021VolumeX64.xml
 shutdown /r /t 1
