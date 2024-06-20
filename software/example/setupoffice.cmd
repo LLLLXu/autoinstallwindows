3567a63c57f7a418133cbe651099a64b2a6f81c4
unknown <lx_forest@hotmail.com>
2024-06-20 18:00:19 +0800

    202406201800

---
+++ software/example/setupoffice.cmd
@@ -0,0 +1,1 @@
+start /wait c:\download\software\office\setup.exe /configure c:\download\software\office\All2021VolumeX64.xml
---
+++ software/example/upgradeacrobat.cmd
@@ -0,0 +1,3 @@
+@REM Download from ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/1500720033/AcroRdrDC1500720033_zh_CN.msi
+@REM Download from ftp://ftp.adobe.com/pub/adobe/reader/win/AcrobatDC/2001320064/AcroRdrDCUpd2001320064.msp
+c:\download\software\AcroRdrDCUpd2001320064.msp /passive /norestart
