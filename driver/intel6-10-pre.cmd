f3da139c0e83f86b73ced505d9075a3806004efa
unknown <lx_forest@hotmail.com>
2024-08-19 17:55:37 +0800

    202408191755

--- driver/intel6-10-pre.cmd
+++ driver/intel6-10-pre.cmd
@@ -1,4 +1,5 @@
 @REM @echo off
 mkdir c:\download\driver\3
 xcopy /e /y o:\autoinstallwindows\driver\public\intel6-10 c:\download\driver\3\intel6-10\
-echo start /wait c:\download\driver\3\intel6-10\Installer.exe /passive /noExtras>c:\download\driver\3\intel6-10.cmd
+@REM echo start /wait c:\download\driver\3\intel6-10\Installer.exe /passive /noExtras>c:\download\driver\3\intel6-10.cmd
+echo start /wait c:\download\driver\3\intel6-10\Installer.exe /passive>c:\download\driver\3\intel6-10.cmd
